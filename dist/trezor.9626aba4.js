bb41e7bad1bce2db05de5ae60ede3c7a4d1ba134
Indrek Lasn <lasnindrek@gmail.com>
2019-08-28 13:53:25 +0300

    master

--- dist/trezor.9626aba4.js
+++ dist/trezor.9626aba4.js
@@ -6164,7 +6164,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "53135" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "53787" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
